a7195eb440025ad56e9a41bef43a10808d699644
guilhermeputnoki <67567620+guilhermeputnoki@users.noreply.github.com>
2022-01-26 16:27:24 -0300

    player pushin the box to the hole complete

---
+++ God Killer/Assets/scripts/hole.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hole : MonoBehaviour
+{
+    public BoxCollider2D bc;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "box")
+        {
+            bc.enabled = false;
+        }
+    }
+}
